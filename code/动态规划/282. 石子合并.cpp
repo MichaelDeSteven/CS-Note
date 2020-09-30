7600bba1664053ac4d3595dcde39cfbc1d21d406
MichaelDeSteven <51652084+MichaelDeSteven@users.noreply.github.com>
2020-09-30 11:49:47 +0800

    Update 282. 石子合并.cpp

--- code/动态规划/282. 石子合并.cpp
+++ code/动态规划/282. 石子合并.cpp
@@ -30,3 +30,36 @@ int main() {
     
     return 0;
 }
+
+
+// 记忆化搜索版本
+#include<bits/stdc++.h>
+
+
+using namespace std;
+const int N = 310;
+int f[N][N], n, s[N];
+
+int dfs(int ll, int rr) {
+    if (f[ll][rr] != -1) return f[ll][rr];
+    if (ll == rr) return f[ll][rr] = 0;
+    
+    int res = 0x3f3f3f;
+    for (int k = ll; k < rr; k++) {
+        res = min(res, dfs(ll, k) + dfs(k + 1, rr) + s[rr] - s[ll - 1]);
+    }
+    return f[ll][rr] = res;
+}
+
+int main() {
+    cin >> n;
+    for (int i = 1; i <= n; i++) {
+        cin >> s[i];
+        s[i] += s[i - 1];
+    }
+    
+    memset(f, -1, sizeof f);
+    
+    cout << dfs(1, n) << endl;
+    return 0;
+}
