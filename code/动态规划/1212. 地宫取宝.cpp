1d8b9f831a1f485f3e10a970a398727411621457
MichaelDeSteven <51652084+MichaelDeSteven@users.noreply.github.com>
2020-09-30 11:26:12 +0800

    Update 1212. 地宫取宝.cpp

--- code/动态规划/1212. 地宫取宝.cpp
+++ code/动态规划/1212. 地宫取宝.cpp
@@ -47,3 +47,49 @@ int main() {
     
     return 0;
 }
+
+
+
+// 记忆化搜索版本
+#include<bits/stdc++.h>
+
+using namespace std;
+const int N = 14, M = 55, MOD = 1000000007;
+typedef long long LL;
+int n, m, k, g[M][M];
+LL f[M][M][N][N];
+
+LL dfs(int u, int v, int keep, int maxV) {
+    if (f[u][v][keep][maxV] != -1) return f[u][v][keep][maxV];
+    if (u == n && v == m) {
+        return f[u][v][keep][maxV] = (keep == k || (keep == k - 1 && g[u][v] > maxV)) ? 1 : 0; 
+    }
+    
+    LL res = 0;
+    
+    if (u + 1 <= n) {
+        res = (res + dfs(u + 1, v, keep, maxV)) % MOD;
+        if (g[u][v] > maxV) res = (res + dfs(u + 1, v, keep + 1, g[u][v])) % MOD;
+    }
+    if (v + 1 <= m) {
+        res = (res + dfs(u, v + 1, keep, maxV)) % MOD;
+        if (g[u][v] > maxV) res = (res + dfs(u, v + 1, keep + 1, g[u][v])) % MOD;
+    }
+        
+    return f[u][v][keep][maxV] = res % MOD;
+}
+int main() {
+    cin >> n >> m >> k;
+    for (int i = 1; i <= n; i++)
+        for (int j = 1; j <= m; j++) {
+            cin >> g[i][j];
+            g[i][j]++;
+        }
+    
+    
+    memset(f, -1, sizeof f);
+    
+    cout << dfs(1, 1, 0, 0) << endl;
+        
+    return 0;
+}
